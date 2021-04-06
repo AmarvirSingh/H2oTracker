7313fb1a3e495c23e01aaa2cfa34f733c3febff7
AmarvirSingh <amarvirgill06@gmail.com>
2021-04-06 13:11:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/h2otracker/StaticAndHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.h2otracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StaticAndHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static_and_history);
+    }
+}
