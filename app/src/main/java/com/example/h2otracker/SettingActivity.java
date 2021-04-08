f5eaaaf620f09d3963168e1f15c8f6784e621ce1
simranaug24 <simranaug24@gmail.com>
2021-04-08 16:50:45 +0530

    SettingActivity

--- app/src/main/java/com/example/h2otracker/SettingActivity.java
+++ app/src/main/java/com/example/h2otracker/SettingActivity.java
@@ -1,14 +1,49 @@
 package com.example.h2otracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SettingActivity extends AppCompatActivity {
+RecyclerView dataList;
+
+List<String> titles;
+List<Integer> images;
+
+Adapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        dataList = findViewById(R.id.datalist);
+
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+
+        titles.add("First item");
+        titles.add("Second item");
+        titles.add("Third item");
+        titles.add("Forth item");
+
+        images.add(R.drawable.ic_baseline_brightness_3_24);
+        images.add(R.drawable.ic_baseline_audiotrack_24);
+        images.add(R.drawable.ic_baseline_favorite_24);
+        images.add(R.drawable.notification);
+
+
+        adapter = new Adapter(this,titles,images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
     }
 }
