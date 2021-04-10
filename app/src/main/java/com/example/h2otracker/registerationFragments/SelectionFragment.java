8b9662871829bf0314e272a58df7b0b9db04b72d
AmarvirSingh <amarvirgill06@gmail.com>
2021-04-10 13:24:45 -0400

    does something

---
+++ app/src/main/java/com/example/h2otracker/registerationFragments/SelectionFragment.java
@@ -0,0 +1,23 @@
+package com.example.h2otracker.registerationFragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.h2otracker.R;
+
+
+public class SelectionFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selection, container, false);
+    }
+}
