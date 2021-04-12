72023f2dbfc84751203822446ffd7d32caf89c1e
AmarvirSingh <amarvirgill06@gmail.com>
2021-04-12 13:38:54 -0400

    integrated firebase

--- app/src/main/java/com/example/h2otracker/LoginActivity.java
+++ app/src/main/java/com/example/h2otracker/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.h2otracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -39,6 +40,16 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setAnimation(animation);
         loginForgotPassword.setAnimation(animation);
         loginCreateAccount.setAnimation(animation);
+
+        loginCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
+
+
 
 
 
