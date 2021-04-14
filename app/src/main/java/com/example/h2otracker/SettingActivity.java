fca08d345dfbf2ef2794345e659bcbb1af129ec9
simranaug24 <simranaug24@gmail.com>
2021-04-15 01:29:34 +0530

    DARK MODE

--- app/src/main/java/com/example/h2otracker/SettingActivity.java
+++ app/src/main/java/com/example/h2otracker/SettingActivity.java
@@ -1,11 +1,15 @@
 package com.example.h2otracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import java.util.ArrayList;
@@ -14,6 +18,8 @@ import java.util.List;
 public class SettingActivity extends AppCompatActivity {
 //RecyclerView dataList;
 
+
+    CardView card1,card2,card3,card4,card5;
 
     private Switch dark_switch;
 List<String> titles;
@@ -26,7 +32,37 @@ Adapter adapter;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        card1 = findViewById(R.id.card1);
        // dataList = findViewById(R.id.datalist);
+
+        //dark mode settings
+        dark_switch = findViewById(R.id.switch3);
+
+        dark_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    card1.setCardBackgroundColor(Color.BLACK);
+
+
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                  //  getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+
+                else
+                {
+                    card1.setBackgroundColor(Color.WHITE);
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+ //                   getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+
+            }
+        });
+
 
 
         titles = new ArrayList<>();
