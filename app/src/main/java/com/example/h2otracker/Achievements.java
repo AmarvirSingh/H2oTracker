fa24f53020a01e6993e34195dd729b3d26b23230
rsdhiman1998 <75512597+rsdhiman1998@users.noreply.github.com>
2021-04-20 12:49:45 -0400

    Achievements page working

--- app/src/main/java/com/example/h2otracker/Achievements.java
+++ app/src/main/java/com/example/h2otracker/Achievements.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 
 import com.example.h2otracker.HelperClass.HelperClass;
 
+import java.util.ArrayList;
+
 public class Achievements extends AppCompatActivity {
     SharedPreferences sharedPreferencesWaterIntake;
     HelperClass helperClass;
@@ -26,7 +28,7 @@ public class Achievements extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
-        img6000 =findViewById(R.id.imageView6000);
+        img6000 = findViewById(R.id.imageView6000);
         img8000 = findViewById(R.id.imageView8000);
         img10000 = findViewById(R.id.imageView10000);
         img12000 = findViewById(R.id.imageView12000);
@@ -37,39 +39,66 @@ public class Achievements extends AppCompatActivity {
         helperClass = new HelperClass(this);
         calculateWaterDrinked();
 
-        if(totalAmount >= 6000 && totalAmount < 8000) {
+        if (totalAmount >= 6000 && totalAmount < 8000) {
             img6000.setVisibility(View.VISIBLE);
-        }
-        else if(totalAmount >= 8000 && totalAmount < 10000)
-        {
+        } else if (totalAmount >= 8000 && totalAmount < 10000) {
             img6000.setVisibility(View.VISIBLE);
             img8000.setVisibility(View.VISIBLE);
 
 
-        }
-        else if(totalAmount >= 10000 && totalAmount < 12000)
-        {
+        } else if (totalAmount >= 10000 && totalAmount < 12000) {
             img6000.setVisibility(View.VISIBLE);
             img8000.setVisibility(View.VISIBLE);
             img10000.setVisibility(View.VISIBLE);
 
 
-        }
-        else if(totalAmount >= 12000 && totalAmount < 15000)
-        {
+        } else if (totalAmount >= 12000 && totalAmount < 15000) {
             img6000.setVisibility(View.VISIBLE);
             img8000.setVisibility(View.VISIBLE);
             img10000.setVisibility(View.VISIBLE);
             img12000.setVisibility(View.VISIBLE);
 
-        }
-        else if(totalAmount >= 15000 && totalAmount < 20000)
-        {
+        } else if (totalAmount >= 15000 && totalAmount < 20000) {
             img6000.setVisibility(View.VISIBLE);
             img8000.setVisibility(View.VISIBLE);
             img10000.setVisibility(View.VISIBLE);
             img12000.setVisibility(View.VISIBLE);
             img15000.setVisibility(View.VISIBLE);
+        } else if (totalAmount >= 20000 && totalAmount < 25000) {
+            img6000.setVisibility(View.VISIBLE);
+            img8000.setVisibility(View.VISIBLE);
+            img10000.setVisibility(View.VISIBLE);
+            img12000.setVisibility(View.VISIBLE);
+            img15000.setVisibility(View.VISIBLE);
+            img20000.setVisibility(View.VISIBLE);
+        } else if (totalAmount >= 25000 && totalAmount < 30000) {
+            img6000.setVisibility(View.VISIBLE);
+            img8000.setVisibility(View.VISIBLE);
+            img10000.setVisibility(View.VISIBLE);
+            img12000.setVisibility(View.VISIBLE);
+            img15000.setVisibility(View.VISIBLE);
+            img20000.setVisibility(View.VISIBLE);
+            img25000.setVisibility(View.VISIBLE);
+        } else if (totalAmount >= 30000) {
+            img6000.setVisibility(View.VISIBLE);
+            img8000.setVisibility(View.VISIBLE);
+            img10000.setVisibility(View.VISIBLE);
+            img12000.setVisibility(View.VISIBLE);
+            img15000.setVisibility(View.VISIBLE);
+            img20000.setVisibility(View.VISIBLE);
+            img25000.setVisibility(View.VISIBLE);
+            img30000.setVisibility(View.VISIBLE);
         }
     }
-}
+        int totalAmount = 0;
+        private void calculateWaterDrinked(){
+            ArrayList<Integer> intakedWater = helperClass.totalIntakeByUser();
+            int a = 0;
+            for (int i= 0; i < intakedWater.size(); i++){
+                a += intakedWater.get(i);
+
+            }
+            totalAmount = a;
+        }
+    }
+
